096b4825a072b7123827bf9c4c6c8eafea61945f
wangzw <wangzw@mail.jj.cn>
2020-04-17 10:03:57 +0800

    optimize

--- MathLib/Program.cs
+++ MathLib/Program.cs
@@ -9,23 +9,37 @@ namespace MathLib
 {
     class Program
     {
+        static int CompareFun(Quad a, Quad b)
+        {
+            if (a.bottomLeft.x < b.bottomLeft.x)
+                return -1;
+            else if (a.bottomLeft.x > b.bottomLeft.x)
+                return 1;
+            return 0;
+        }
+
         static void Main(string[] args)
         {
             Dictionary<int, int> quadOverlayMap = new Dictionary<int, int>();       //每个四边形与其它四边形相交的次数
             int count = 10000;
-            Quad[] quads = new Quad[count];
+            List<Quad> quads = new List<Quad>();
             var random = new Random(new Guid().GetHashCode());
             for (int i = 0; i < count; i++)
             {
                 var bl = new Point(random.Next(-1000, -10), random.Next(-1000, -10));
                 var tr = new Point(bl.x + 100, bl.y + 100);
-                quads[i] = new Quad(bl, tr);
+                quads.Add(new Quad(bl, tr));
             }
+
+            //先排序，按左下角x位置排序
+            quads.Sort(CompareFun);
+
             for (int i = 0; i < count - 1; i++)
             {
                 var quad = quads[i];
                 for (int j = i + 1; j < count; j++)
                 {
+                    if (quad.topRight.x < quads[j].bottomLeft.x) continue;
                     if(Quad.Intersect(quad, quads[j]))
                     {
                         if(quadOverlayMap.ContainsKey(i))
